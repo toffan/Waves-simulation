6027942a84e57ae272a0c209d29195323590c1dc
mistiru <anael.megna@hotmail.fr>
2015-05-30 18:35:20 +0200

    Hack Reconstruction

--- Sources_phase2/Reconstruction_fortran.m
+++ Sources_phase2/Reconstruction_fortran.m
@@ -32,7 +32,7 @@ if (check)
     return
   end
   converged=1;
-  while (D(converged)/D(1)>1-percentInfo) 
+  while (D(converged)/D(1)>1-percentInfo)
     converged=converged+1;
   end
   converged=converged-1;
@@ -40,7 +40,7 @@ if (check)
   time = toc;
   fprintf('dimension of the subspace: %d\n temps: %7.3f seconds',converged,time);
 else
-  addpath('Fortran') 
+  addpath('Fortran')
   p=1;
   m=10;
   maxit=300;
@@ -50,13 +50,19 @@ else
   [U,d] = fortran_subspace_iter_sv(Z(43:46:sup_m,:),m,p,percentInfo,eps,maxit);
   % [U,d] = fortran_subspace_iter_sv(Z,m,p,percentInfo,eps,maxit);
   [m,n] = size(U);
-  U = [U;zeros(sup_m-m,n)];
+  super_U = zeros(sup_m-42,n);
+  for i=1:m
+    if (mod(i,3) < 1)
+      super_U(3*(i-1) + 1,:) = U(i,:);
+    end
+  end
+  U = [zero(42,n);super_U];
   time=toc;
   converged=size(d,1);
   condition=1-d(end)/d(1);
   fprintf(['%d sing. values found in %7.3f seconds ; they ' ...
            'provide %3.2f%% variability. \n'], converged, time,condition);
- 
+
 end
 
 
@@ -64,7 +70,7 @@ end
 [X, ns, nt] = Model(GW,1);
 X0 = X(1:ns,:);
 %%%%
-Zp = zeros(size(X)); 
+Zp = zeros(size(X));
 %%%%
 Z0=X0-muF(1:ns);
 alpha=(U(1:ns,:)'*U(1:ns,:))\(U(1:ns,:)'*Z0);
@@ -83,7 +89,7 @@ global Lx Ly Nx Ny;
     y = linspace(0,Ly,Ny);     %  Independent variable y
     [Mx, My] = meshgrid(x,y);  %  2D arrays, mainly for plotting
     Mx = Mx'; My = My';        %  MatLab is strange!
-    
+
       figure(2)
  for tt=1:nt
           subplot(1,2,1);
@@ -93,7 +99,7 @@ global Lx Ly Nx Ny;
           axis([0,Lx,0,Ly ,5000,6000]);
 	  pbaspect([3 1 3])
 	  title('Solution')
-	  
+
           subplot(1,2,2);
           zappr = Xp((tt-1)*ns+1:tt*ns,1);
           zappr = reshape(zappr,Nx,Ny);
